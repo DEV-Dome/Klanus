0f678e6bfbbe7116c4047251996d01db070a206c
Dominic Backhaus <DominicBackhaus@web.de>
2022-03-06 11:04:15 +0100

    Projekt Übersicht fertig

--- pages/main/userDashboard.php
+++ pages/main/userDashboard.php
@@ -24,56 +24,32 @@ $rang = new Rang($_SESSION['Rang'], $pdo);
 
 <!-- TODO Datenbank-->
 <div class="flexbox">
+    <?php
+    //TODO: Projeckt wo man mitgild ist hinzufügen
+    $sth = $pdo->prepare("SELECT * FROM projekt WHERE Besitzer  = ? LIMIT 5");
+    $sth->bindParam(1, $_SESSION['ID']);
+    $sth->execute();
 
-    <div class="dashbordProjektKasten">
-        <span class="Projekttool"><i  style="color: #fff408;" class="bi bi-x-diamond-fill"></i></span>
-        <span class="Projekttool Projekttoolright"><i class="bi bi-heart"></i></span>
+    foreach ($sth->fetchAll() as $row) {
+        ?>
+        <div class="dashbordProjektKasten">
+            <span class="Projekttool"><i  style="color: #fff408;" class="bi bi-x-diamond-fill"></i></span>
+            <span class="Projekttool Projekttoolright"><i class="bi bi-heart"></i></span>
 
-        <img class="dashbordProjektImg" src="assets/default_projeckt.png">
+            <img class="dashbordProjektImg" src="assets/default_projeckt.png">
 
-        <p class="dashbordProjektName">Name</p>
-        <button class="dashbordProjektButton">Zum Projekt</button>
-    </div>
-
-    <div class="dashbordProjektKasten">
-        <span class="Projekttool"><i  style="color: #fff408;" class="bi bi-x-diamond-fill"></i></span>
-        <span class="Projekttool Projekttoolright"><i class="bi bi-heart"></i></span>
-
-        <img class="dashbordProjektImg" src="assets/default_projeckt.png">
-
-        <p class="dashbordProjektName">Name</p>
-        <button class="dashbordProjektButton">Zum Projekt</button>
-    </div>
-
-    <div class="dashbordProjektKasten">
-        <span class="Projekttool"><i  style="color: #fff408;" class="bi bi-x-diamond-fill"></i></span>
-        <span class="Projekttool Projekttoolright"><i class="bi bi-heart"></i></span>
-
-        <img class="dashbordProjektImg" src="assets/default_projeckt.png">
-
-        <p class="dashbordProjektName">Name</p>
-        <button class="dashbordProjektButton">Zum Projekt</button>
-    </div>
-
-    <div class="dashbordProjektKasten">
-        <span class="Projekttool"><i  style="color: #fff408;" class="bi bi-x-diamond-fill"></i></span>
-        <span class="Projekttool Projekttoolright"><i class="bi bi-heart"></i></span>
-
-        <img class="dashbordProjektImg" src="assets/default_projeckt.png">
-
-        <p class="dashbordProjektName">Name</p>
-        <button class="dashbordProjektButton">Zum Projekt</button>
-    </div>
-
-    <div class="dashbordProjektKasten">
-        <span class="Projekttool"><i  style="color: #fff408;" class="bi bi-x-diamond-fill"></i></span>
-        <span class="Projekttool Projekttoolright"><i class="bi bi-heart"></i></span>
-
-        <img class="dashbordProjektImg" src="assets/default_projeckt.png">
-
-        <p class="dashbordProjektName">Name</p>
-        <button class="dashbordProjektButton">Zum Projekt</button>
-    </div>
+            <p class="dashbordProjektName"><?php echo $row["Name"]; if($row["Verifiziert"]) { ?> <i style="color: #45FF58;" class="bi bi-check2-circle"></i> <?php }?></p>
+            <button class="dashbordProjektButton">Zum Projekt</button>
+        </div>
+        <?php
+    }
+    ?>
 </div>
+
+<div class="buttonMoreProjektsContainer">
+    <!-- Mehr Button-->
+    <i class="bi bi-chevron-compact-down buttonMoreProjekts"></i>
+</div>
+
 
 
