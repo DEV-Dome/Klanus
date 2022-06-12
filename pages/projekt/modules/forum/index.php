81cf295a2e8f80ad937742663e97105752ed6095
Dominic Backhaus <DominicBackhaus@web.de>
2022-06-12 15:08:00 +0200

    Mobil Ansicht angepasst

--- pages/projekt/modules/forum/index.php
+++ pages/projekt/modules/forum/index.php
@@ -16,8 +16,14 @@ include_once "../../../../php/rang/Rang.php";
 
 $rang = new Rang($_SESSION['Rang'], $pdo);
 $prang = new projektRang($_SESSION['PRang'], $pdo)
+
+/*
+ * Name: 50 Zeichen
+ * Beschreibung: 50 Zeichen
+ */
 ?>
 <link href="pages/projekt/modules/forum/css/main.css" rel="stylesheet">
+<link href="pages/projekt/modules/forum/css/main_handy.css" rel="stylesheet">
 
 <div id="main_forum">
     <div class="Forum_Kategorie">
@@ -34,8 +40,7 @@ $prang = new projektRang($_SESSION['PRang'], $pdo)
                 Test-Forum
             </div>
             <div class="Forum_beschreibung">
-                Tolle beschreibung
-            </div>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea tak            </div>
         </div>
         <div class="Forum">
             <div class="Forum_icon">
