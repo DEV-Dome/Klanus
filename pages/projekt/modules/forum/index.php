749b3bda2de1e44d708917eca8f4cd34c42f4d9e
Dominic Backhaus <DominicBackhaus@web.de>
2022-06-29 18:09:14 +0200

    kleine Änderungen

--- pages/projekt/modules/forum/index.php
+++ pages/projekt/modules/forum/index.php
@@ -18,15 +18,14 @@ $rang = new Rang($_SESSION['Rang'], $pdo);
 $prang = new projektRang($_SESSION['PRang'], $pdo);
 $prang_prioritat = 0;
 
-    $sqlstr = "SELECT *  FROM projekt_rang WHERE ID = ? ";
+    $sqlstr = "SELECT * FROM projekt_rang WHERE ID = ? ";
     $sth = $pdo->prepare($sqlstr);
     $sth->bindParam(1, $_SESSION['PRang']);
     $sth->execute();
 
     foreach($sth->fetchAll() as $row) {
-        $prang_prioritat = $_SESSION['PRang'];
+        $prang_prioritat = $row['Prioritat'];
     }
-    echo $prang_prioritat;
 ?>
 <link href="pages/projekt/modules/forum/css/main.css?v=<?php echo time() ?>" rel="stylesheet">
 <link href="pages/projekt/modules/forum/css/main_handy.css?v=<?php echo time() ?>" rel="stylesheet">
@@ -36,10 +35,11 @@ $prang_prioritat = 0;
     <?php
 
     $sqlstr = "SELECT *,projekt_forum_kategorien.Name AS 'kname',projekt_forum_kategorien.ID 'kID'  FROM projekt_forum_kategorien,projekt_rang WHERE  ";
-    $sqlstr .= "projekt_forum_kategorien.Rang = projekt_rang.ID AND projekt_forum_kategorien.Projekt = ? AND projekt_rang.Prioritat <= $prang_prioritat ";
+    $sqlstr .= "projekt_forum_kategorien.Rang = projekt_rang.ID AND projekt_forum_kategorien.Projekt = ? AND projekt_rang.Prioritat <= ? ";
     $sqlstr .= "ORDER BY projekt_forum_kategorien.prioritat ASC";
     $sth = $pdo->prepare($sqlstr);
     $sth->bindParam(1, $_SESSION["projekt.aktiv"]);
+    $sth->bindParam(2, $prang_prioritat);
     $sth->execute();
 
     foreach($sth->fetchAll() as $row) {
@@ -53,10 +53,11 @@ $prang_prioritat = 0;
 
             <?php
             $sqlstr1 = "SELECT *,projekt_forum_forn.Name AS 'fName',projekt_forum_forn.ID AS 'fID'  FROM projekt_forum_forn,projekt_rang ";
-            $sqlstr1 .= "WHERE KannSehen = projekt_rang.ID AND kategorien = ? AND projekt_rang.Prioritat <= $prang_prioritat ";
+            $sqlstr1 .= "WHERE KannSehen = projekt_rang.ID AND kategorien = ? AND projekt_rang.Prioritat <= ?";
             $sqlstr1 .= "ORDER BY projekt_forum_forn.prioritat ASC";
             $sth_fornen = $pdo->prepare($sqlstr1);
             $sth_fornen->bindParam(1, $row["kID"]);
+            $sth_fornen->bindParam(2, $prang_prioritat);
             $sth_fornen->execute();
 
             foreach($sth_fornen->fetchAll() as $row_foren) {
